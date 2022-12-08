c12eb8c8a09bb21c8d2e4d03ded6bc9e008a41d0
JackJones2131 <shankar@refreshtechlabs.com>
2022-12-08 16:08:37 +0530

    package brand export cleared

--- lib/flutter_credit_card_ui.dart
+++ lib/flutter_credit_card_ui.dart
@@ -3,3 +3,4 @@ export 'credit_card_ui_model.dart';
 export 'credit_card_ui_widget.dart';
 export 'custom_card_type_icon.dart';
 export 'glassmorphism_config.dart';
+export 'credit_card_ui_brand.dart';
